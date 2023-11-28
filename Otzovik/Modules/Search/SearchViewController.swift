31233781cea2a9a06329cf92d6c1d83ab82f6a03
ukidoshi <nsaryglar200@gmail.com>
2023-11-28 15:13:40 +0300

    search view controller | Teacher table view

--- Otzovik/Modules/Search/SearchViewController.swift
+++ Otzovik/Modules/Search/SearchViewController.swift
@@ -3,6 +3,11 @@ import UIKit
 // MARK: 1st VC
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
+    private let tableView = UITableView()
+    
+    private var teachers: [Teacher] = []
+    private let teacherManager = TeacherManager()
+    
     private var ChangeUniversityButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Изменить", for: .normal)
@@ -76,7 +81,75 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
                                                selector: #selector(keyboardWillHide(notification:)),
                                                name: UIResponder.keyboardWillHideNotification,
                                                object: nil)
+        
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: "SearchTableViewCell")
+        loadData()
+        view.addSubview(tableView)
+        
         super.viewDidLoad()
     }
     
+    func loadData() {
+        teacherManager.loadTeachers { [weak self] teachers in
+            self?.teachers = teachers
+            self?.tableView.reloadData()
+        }
+    }
+    
+}
+
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teachers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath)
+        let mainCell = cell as? SearchTableViewCell
+        
+        mainCell?.configure(with: teachers[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print(sourceIndexPath, destinationIndexPath)
+        let sourceProduct = teachers[sourceIndexPath.row]
+        teachers.remove(at: sourceIndexPath.row)
+        teachers.insert(sourceProduct, at: destinationIndexPath.row)
+    }
+   
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {
+            return
+        }
+        
+        teachers.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 76
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let teacherViewController = TeacherViewController(teacher: teachers[indexPath.row])
+        let navigationController = UINavigationController(rootViewController: teacherViewController)
+        
+        present(navigationController, animated: true)
+    }
 }
