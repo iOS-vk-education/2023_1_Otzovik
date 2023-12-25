13e945ef4a39f8d139a51a2cbe1acda9eebac80d
m1rosh <mirosh430@gmail.com>
2023-12-25 16:03:22 +0300

    testviewcontroller deleted

--- Otzovik/Modules/Search/TestViewController.swift
+++ Otzovik/Modules/Search/TestViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Сергей Мирошниченко on 25.12.2023.
 //
 
-import Foundation
+import UIKit
+protocol TestDeligate: AnyObject{
+    func didReceiveData(_ data: String)
+}
+class TestViewController: UIViewController{
+    weak var delegate: TestDeligate?
+    var activityIndicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.center = view.center
+        //activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        activityIndicator.stopAnimating()
+        delegate?.didReceiveData("data")
+    }
+}
